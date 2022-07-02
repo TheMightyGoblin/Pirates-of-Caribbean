e8f9b13e6275402c5564550487c9668bd3a5bebc
FynsonSOS <yanmuifa@yahoo.com.hk>
2022-07-02 14:46:17 +0800

    Particle effect, screen shake and scrolling background

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
+    [SerializeField] bool applyScreenShake;
+    ScreenShake screenShake;
+    void Awake()
+    {
+        screenShake = Camera.main.GetComponent<ScreenShake>();
+    }
     
     void OnTriggerEnter2D(Collider2D other) 
     {
@@ -13,6 +20,8 @@ public class Health : MonoBehaviour
         if(damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            ShakeScreen();
+            PlayHitEffect();
             damageDealer.Hit();
         }
     }
@@ -23,6 +32,23 @@ public class Health : MonoBehaviour
         if(health <= Mathf.Epsilon)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect()
+    {
+        if(hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position ,Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
+    }
+
+    void ShakeScreen()
+    {
+        if(screenShake != null && applyScreenShake)
+        {
+            screenShake.Play();
         }
     }
 }
